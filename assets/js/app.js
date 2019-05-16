201af9fcf698f4f862ff2f8397e1c3f332ac7e39
Joel Sotelo <joel.pilgrim88@gmail.com>
2019-05-15 19:17:42 -0500

    Added the code of the svg chart example

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,21 @@
-// @TODO: YOUR CODE HERE!
+
+
+var svg = d3.select("#scatter").append("svg");
+
+svg.attr("width", "100px").attr("height", "100px");
+
+var circles = svg.selectAll("circle");
+
+var rValues = [40, 25, 10];
+
+circles.data(rValues)
+    .enter()
+    .append("circle")
+    .attr("cx", 50)
+    .attr("cy", 50)
+    .attr("r", function(d) {
+      return d;
+    })
+    .attr("stroke", "black")
+    .attr("stroke-width", "5")
+    .attr("fill", "red");
